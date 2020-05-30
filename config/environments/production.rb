3206cf82305f4ae3a37461e95d5e18165ba8a71f
Zeisho <dream_08026672964@yahoo.co.jp>
2020-05-30 02:10:55 +0000

    production.rb address sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   host = 'https://stock-manager-zeisho.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.setting.net',
+    :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :authentication       => :plain,
     :user_name            => ENV['SENDGRID_USERNAME'],
