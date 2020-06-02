9f0823bb118994be4f254c7353cc2259db4178e3
Zeisho <dream_08026672964@yahoo.co.jp>
2020-06-02 02:54:17 +0000

    Admin user config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
 User.create!(name: "Example User",
-             email: "example@railstutorial.org",
+             email: "mazdarx808026672964@gmail.com",
              password: "foobar",
              password_confirmation: "foobar",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
              
-99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
+# 99.times do |n|
+#   name = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name: name,
+#               email: email,
+#               password: password,
+#               password_confirmation: password,
+#               activated: true,
+#               activated_at: Time.zone.now)
+# end
                
-users = User.order(:created_at).take(6)
-50.times do
-  name = Faker::Lorem.sentence(2)
-  users.each { |user| user.stocks.create!(name: name) }
-end
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   name = Faker::Lorem.sentence(2)
+#   users.each { |user| user.stocks.create!(name: name) }
+# end
