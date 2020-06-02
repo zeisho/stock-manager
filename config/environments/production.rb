12f6cb757c4165186c718c9583c776fbe4b83634
Zeisho <dream_08026672964@yahoo.co.jp>
2020-06-02 03:04:46 +0000

    App name change zeisho-stock-manager

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://stock-manager-zeisho.herokuapp.com'
+  host = 'https://zeisho-stock-manager.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
